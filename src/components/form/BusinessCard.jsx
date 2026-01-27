63107679d95e5d71a8bd2229cf4ad55e0e58adbf
Simone <info@simoneicardi.com>
2026-01-27 12:05:32 +0100

    centralizzazione su singola func per onchange e aggiunta nomi realtivi a campi

--- src/components/form/BusinessCard.jsx
+++ src/components/form/BusinessCard.jsx
@@ -23,6 +23,17 @@ function BusinessCard() {
         companyPic: ""
     });
 
+    // funzione di gestione dei change dei vari input
+
+    function handleFormData(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+
+
+    }
+
     return (
         <div className="container mt-3">
             <div className="row">
@@ -32,65 +43,72 @@ function BusinessCard() {
                         <div className="row g-3 row-cols-2">
                             <div className="col">
                                 <input
+                                    name="firstName"
                                     className="form-control"
                                     placeholder="Nome"
                                     type="text"
                                     value={formData.firstName}
-                                    onChange={(e) => setFormData({ ...formData, firstName: e.target.value })}
+                                    onChange={handleFormData}
                                 />
                             </div>
                             <div className="col">
                                 <input
+                                    name="lastName"
                                     className="form-control"
                                     placeholder="Cognome"
                                     type="text"
                                     value={formData.lastName}
-                                    onChange={(e) => setFormData({ ...formData, lastName: e.target.value })}
+                                    onChange={handleFormData}
                                 />
                             </div>
                             <div className="col">
                                 <input
+                                    name="companyName"
                                     className="form-control"
                                     placeholder="Azienda"
                                     type="text"
                                     value={formData.companyName}
-                                    onChange={(e) => setCompanyName(e.target.value)}
+                                    onChange={handleFormData}
                                 />
                             </div>
                             <div className="col">
                                 <input
+                                    name="role"
                                     className="form-control"
                                     placeholder="Ruolo"
                                     type="text"
                                     value={formData.role}
-                                    onChange={(e) => setRole(e.target.value)}
+                                    onChange={handleFormData}
                                 />
                             </div>
                             <div className="col">
                                 <input
+                                    name="email"
                                     className="form-control"
                                     placeholder="Email"
                                     type="email"
                                     value={formData.email}
-                                    onChange={(e) => setEmail(e.target.value)}
+                                    onChange={handleFormData}
                                 />
                             </div>
                             <div className="col">
                                 <input
+                                    name="phone"
                                     className="form-control"
                                     placeholder="Telefono"
                                     type="tel"
                                     value={formData.phone}
-                                    onChange={(e) => setPhone(e.target.value)}
+                                    onChange={handleFormData}
                                 />
                             </div>
                         </div>
                         <input
+                            name="companyPic"
                             className="form-control mt-3"
                             placeholder="Logo Azienda"
                             type="url"
                             value={formData.companyPic}
-                            onChange={(e) => setCompanyPic(e.target.value)}
+                            onChange={handleFormData}
                         />
                     </form>
                 </div>
