af29c65e51255780fefd08bc1c8dbaa23f668e22
Simone <info@simoneicardi.com>
2026-01-29 11:27:54 +0100

    aggiungiamo chiamata dettaglio personaggio API

--- src/pages/CharachterDetail.jsx
+++ src/pages/CharachterDetail.jsx
@@ -1,8 +1,22 @@
-// importiamo componente di listato
+// importiamo axios
+import axios from "axios";
+// importiamo hooks per gestione chiamata e recupero parametro dinamico
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+const endpointBase = "https://rickandmortyapi.com/api/character/";
+
 function CharachterDetail() {
+    // recuper valore param dinamico grazie a hook useParams
     const { id } = useParams();
+
+    // utilizzo il parametro per la chiamata corretta (sempre passando da hook su primo montaggio)
+    useEffect(() => {
+        axios.get(endpointBase + id)
+            .then(resp => console.log(resp.data))
+            .catch(err => console.log("errore sulla chiamata", err))
+    }, [])
+
 
     return (
         <>
