fc7354b64c3119c2b1fb8469e4323b030f0c6182
Simone <info@simoneicardi.com>
2026-01-30 12:56:21 +0100

    aggiungi logica in home per listato prefereriti

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom"
 
+// importiamo il context
+import { useFavortite } from "../contexts/FavoriteContext"
+// importiamo comp card listato
+import CharacterCard from "../components/rickmorty/CharacterCard"
+
 function HomePage() {
+
+    // usiamo il context importato
+    const { favorites, characters } = useFavortite();
+
+    // creiamo versione filtrato con solo personaggi preferiti
+    const favoriteCharacters = characters.filter(character => {
+        return favorites.includes(character.id)
+    });
 
     return (
         <>
@@ -12,6 +25,19 @@ function HomePage() {
             <Link to="/personaggi" className="btn btn-primary">
                 Vai alla lista dei personaggi
             </Link>
+
+            <h3>I tuoi personaggi preferiti</h3>
+            {favorites.length ? (
+                <div className="characters">
+                    {favoriteCharacters.map(character => (
+                        <CharacterCard key={character.id}
+                            character={character} />
+                    ))}
+                </div>
+            ) : (
+                <p>Non hai ancora aggiunto personaggi ai preferiti.</p>
+            )}
+
         </>
     )
 }
